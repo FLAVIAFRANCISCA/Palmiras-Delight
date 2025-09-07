40469a7aed3c273108b2685027d6629481d29032
Flavia <Franciscakapenda@gmail.com>
2025-09-07 18:49:52 +0200

    added new items

--- script.js
+++ script.js
@@ -70,7 +70,9 @@ const galleryImages = [
   { src: 'images/cake16.jpg', label: 'Baby Shower Cake' },
   { src: 'images/cake17.jpg', label: 'Baby Shower Cake' },
   { src: 'images/cake18.jpg', label: 'Rainbow Cake' },
-  { src: 'images/cake19.jpg', label: 'Spider Webs Cake' }
+  { src: 'images/cake19.jpg', label: 'Spider Webs Cake' },
+  { src: 'images/cake20.jpg', label: 'Fruit Deco Cake' }
+  
 ];
 
 galleryImages.forEach(imgObj => {
